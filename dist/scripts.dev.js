899ebecac6ea9c6e69cf2e88b44381d2bb531240
Anuj Mathur <amathur@marchnetworks.com>
2020-08-22 11:16:24 -0400

    quotes + nav bar

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -1,9 +1,11 @@
 "use strict";
 
-var numb = Math.floor(Math.random() * 1643);
-fetch("https://type.fit/api/quotes").then(function (response) {
-  return response.json();
-}).then(function (data) {
-  document.getElementById("quote").innerHTML = data[numb].text;
-  document.getElementById("author").innerHTML = data[numb].author;
-});
+var reloadQuote = function reloadQuote() {
+  var numb = Math.floor(Math.random() * 1643);
+  fetch("https://type.fit/api/quotes").then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    document.getElementById("quote").innerHTML = data[numb].text;
+    document.getElementById("author").innerHTML = data[numb].author;
+  });
+};
--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
+var reloadQuote = function(){ 
 var numb = Math.floor(Math.random() * 1643);
-
 fetch("https://type.fit/api/quotes").then(function (response) {
     return response.json();
 }).then(function (data) {
     document.getElementById("quote").innerHTML = data[numb].text;
     document.getElementById("author").innerHTML = data[numb].author;
 });
+}
