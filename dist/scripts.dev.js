314bbab457bfb86627526e7270d336ead661a33a
Anuj Mathur <amathur@marchnetworks.com>
2020-08-22 11:27:25 -0400

    updating CSS

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -5,7 +5,7 @@ var reloadQuote = function reloadQuote() {
   fetch("https://type.fit/api/quotes").then(function (response) {
     return response.json();
   }).then(function (data) {
-    document.getElementById("quote").innerHTML = data[numb].text;
+    document.getElementById("quotes").innerHTML = data[numb].text;
     document.getElementById("author").innerHTML = data[numb].author;
   });
 };
--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ var numb = Math.floor(Math.random() * 1643);
 fetch("https://type.fit/api/quotes").then(function (response) {
     return response.json();
 }).then(function (data) {
-    document.getElementById("quote").innerHTML = data[numb].text;
+    document.getElementById("quotes").innerHTML = data[numb].text;
     document.getElementById("author").innerHTML = data[numb].author;
 });
 }
