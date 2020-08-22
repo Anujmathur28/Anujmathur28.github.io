02d509ca63d4c7c5c239cfee5249af02dc3eb66b
Anuj Mathur <amathur@marchnetworks.com>
2020-08-21 20:05:23 -0400

    get quote api

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var numb = Math.floor(Math.random() * 1643);
+
+fetch("https://type.fit/api/quotes").then(function (response) {
+    return response.json();
+}).then(function (data) {
+    document.getElementById("quote").innerHTML = data[numb].text;
+    document.getElementById("author").innerHTML = data[numb].author;
+});
