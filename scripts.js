dc9135ff63c826e29847cc5a7ada0db99069dbb4
Anuj Mathur <amathur@marchnetworks.com>
2020-08-23 19:29:55 -0400

    API Call to Google Places API

--- scripts.js
+++ scripts.js
@@ -7,3 +7,51 @@ var reloadQuote = function () {
         document.getElementById("author").innerHTML = data[numb].author;
     });
 }
+
+let photosHeight;
+let photosHtml;
+let photosReference;
+let photosWidth;
+
+//var getImage = function () {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    let url =`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyA2tLUogp1e_tnALcAO1-v_PLhcxdedoxM`;
+    fetch(proxyUrl + url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        //if (!error && response.statusCode == 200) {
+            
+           var photosObject = data.candidates[0].photos[0];
+                      
+           photosHeight = photosObject.height;
+           photosHtml = photosObject.html_attributions;
+           photosReference = photosObject.photo_reference;
+           photosWidth = photosObject.width;
+           
+   // }
+    });
+//}
+
+/*
+import * as request from 'request'; //request module
+import * as fs from 'fs'; //writing to output 
+var png = require('console-png'); */
+/*
+let picture = {
+    url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=${photosWidth}&photoreference=${photosReference}&key=AIzaSyA2tLUogp1e_tnALcAO1-v_PLhcxdedoxM`,
+    method: 'GET' //export the dashboard specified with the dashboard id in the url above
+};
+
+const writeToFile = (body: string) => {
+    fs.writeFile(`./image4.png`, body, function(err) { //writing to the json file
+        console.log("The file was saved!");
+   }); 
+}
+
+const image = (error: string, response: { statusCode: number; }, body: string) => {
+    //if (!error && response.statusCode == 200) {
+         
+    writeToFile(body); 
+//}
+}
+request(picture, image); */
