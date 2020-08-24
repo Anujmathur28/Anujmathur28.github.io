b644d52873f0bebf347e07ea3162943ddeb6891f
Anuj Mathur <amathur@marchnetworks.com>
2020-08-24 16:11:13 -0400

    search works

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -9,43 +9,77 @@ var reloadQuote = function reloadQuote() {
     document.getElementById("author").innerHTML = data[numb].author;
   });
 };
+/**************************************************************************/
+
 
 var imageGame = function imageGame() {
-  //var wnd = window.open("", "funzone");
   var photosHeight;
   var photosHtml;
   var photosReference;
   var photosWidth;
   var placeId;
   var key = 'AIzaSyA2tLUogp1e_tnALcAO1-v_PLhcxdedoxM';
-  var inputText = "statue of liberty";
+  var inputText = document.getElementById("searchTextField").value;
   var proxyUrl = "https://cors-anywhere.herokuapp.com/";
   var queryUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=".concat(inputText, "&inputtype=textquery&fields=photos,geometry,place_id,type,formatted_address,name,opening_hours,rating&key=").concat(key);
   fetch(proxyUrl + queryUrl).then(function (response) {
     return response.json();
   }).then(function (data) {
     var photosObject = data.candidates[0].photos[0];
-    placeId = "'" + data.candidates[0].place_id + "'"; //console.log(placeId);
-
+    placeId = data.candidates[0].place_id;
     photosHeight = photosObject.height;
     photosHtml = photosObject.html_attributions;
     photosReference = photosObject.photo_reference;
-    photosWidth = photosObject.width; //document = "./funzone.html";
-
+    photosWidth = photosObject.width;
     var text = '<img src = ';
     var imgUrl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=".concat(photosReference, "&key=").concat(key);
     var text2 = " id=cd alt=".concat(photosReference, "></img>");
     var totalText = text + imgUrl + text2;
-    document.getElementById("photo").innerHTML = totalText; // }
-  }); //}
-  //var getImage = function () {
-
-  placeId = 'ChIJN1t_tDeuEmsRUsoyG83frY4';
-  var imgUrl = "https://maps.googleapis.com/maps/api/place/details/json?place_id=".concat(placeId, "&fields=icon,photo,name,rating&key=").concat(key);
-  fetch(proxyUrl + imgUrl).then(function (response) {
-    return response.json();
+    document.getElementById("photo").innerHTML = totalText;
   }).then(function (data) {
-    //if (!error && response.statusCode == 200) {
-    console.log(data); // }
-  }); //}
-};
+    var imgUrl = "https://maps.googleapis.com/maps/api/place/details/json?place_id=".concat(placeId, "&fields=icon,photo,name,rating&key=").concat(key);
+    fetch(proxyUrl + imgUrl).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      var offset = 0;
+      var photoArray = data.result.photos;
+
+      var _loop = function _loop(index) {
+        var reference = photoArray[index].photo_reference;
+        setTimeout(function () {
+          console.log(reference);
+          var text1 = '<img src = ';
+          var imgUrl1 = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=".concat(reference, "&key=").concat(key);
+          var text21 = " id=cd alt=".concat(reference, "></img>");
+          var totalText1 = text1 + imgUrl1 + text21;
+          document.getElementById("photo").innerHTML = totalText1;
+        }, 2000 + offset);
+        offset += 2000;
+      };
+
+      for (var index = 0; index < photoArray.length; index++) {
+        _loop(index);
+      }
+    });
+  });
+}; //let imageGames(){
+//  console.log()
+//}
+
+/* 
+                    let text1 = '<img src = ';
+                    let imgUrl1 = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=${key}`;
+                    let text21 = ` id=cd alt=${reference}></img>`;
+                    let totalText1 = text1 + imgUrl1 + text21;
+                    
+                    document.getElementById("photo").innerHTML = totalText1;*/
+
+/*********************************************************************************/
+
+
+function initialize() {
+  var input = document.getElementById('searchTextField');
+  new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
--- scripts.js
+++ scripts.js
@@ -7,52 +7,78 @@ var reloadQuote = function () {
         document.getElementById("author").innerHTML = data[numb].author;
     });
 }
-
-var imageGame = function ()  {
-    //var wnd = window.open("", "funzone");
+/**************************************************************************/
+var imageGame = function () {
     let photosHeight;
     let photosHtml;
     let photosReference;
     let photosWidth;
     let placeId;
     const key = 'AIzaSyA2tLUogp1e_tnALcAO1-v_PLhcxdedoxM';
-    const inputText = "statue of liberty";
+    const inputText = document.getElementById("searchTextField").value;
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-let queryUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${inputText}&inputtype=textquery&fields=photos,geometry,place_id,type,formatted_address,name,opening_hours,rating&key=${key}`;
-fetch(proxyUrl + queryUrl).then(function (response) {
-    return response.json();
-    
-}).then(function (data) {
-    var photosObject = data.candidates[0].photos[0];
-    placeId = "'" + data.candidates[0].place_id + "'";
-    //console.log(placeId);
-    photosHeight = photosObject.height;
-    photosHtml = photosObject.html_attributions;
-    photosReference = photosObject.photo_reference;
-    photosWidth = photosObject.width;
-    //document = "./funzone.html";
-    let text = '<img src = ';
-    let imgUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photosReference}&key=${key}`;
-    let text2 = ` id=cd alt=${photosReference}></img>`;
-    let totalText = text + imgUrl + text2;
-    
-    document.getElementById("photo").innerHTML = totalText;
+    let queryUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${inputText}&inputtype=textquery&fields=photos,geometry,place_id,type,formatted_address,name,opening_hours,rating&key=${key}`;
 
-    // }
-});
-//}
+    fetch(proxyUrl + queryUrl).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            var photosObject = data.candidates[0].photos[0];
+            placeId = data.candidates[0].place_id;
+            photosHeight = photosObject.height;
+            photosHtml = photosObject.html_attributions;
+            photosReference = photosObject.photo_reference;
+            photosWidth = photosObject.width;
 
-//var getImage = function () {
-    placeId = 'ChIJN1t_tDeuEmsRUsoyG83frY4';
-    let imgUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=icon,photo,name,rating&key=${key}`;
-    fetch(proxyUrl + imgUrl).then(function (response) {
-        return response.json();
-        
-    }).then(function (data) {
-        //if (!error && response.statusCode == 200) {
-        console.log(data);
-            
-        // }
-    });
-    //}
+            let text = '<img src = ';
+            let imgUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photosReference}&key=${key}`;
+            let text2 = ` id=cd alt=${photosReference}></img>`;
+            let totalText = text + imgUrl + text2;
+
+            document.getElementById("photo").innerHTML = totalText;
+
+        })
+        .then(function (data) {
+            let imgUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=icon,photo,name,rating&key=${key}`;
+
+            fetch(proxyUrl + imgUrl).then(function (response) {
+                return response.json();
+
+            }).then(function (data) {
+                let offset = 0;
+                let photoArray = data.result.photos;
+                for (let index = 0; index < photoArray.length; index++) {
+                    let reference = photoArray[index].photo_reference;
+                    setTimeout(function(){
+                        console.log(reference);
+                        let text1 = '<img src = ';
+                    let imgUrl1 = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=${key}`;
+                    let text21 = ` id=cd alt=${reference}></img>`;
+                    let totalText1 = text1 + imgUrl1 + text21;
+                    
+                    document.getElementById("photo").innerHTML = totalText1;
+                        
+                      }, 2000 + offset);    
+                     offset += 2000;
+                }
+                
+            });
+        });
 }
+
+//let imageGames(){
+  //  console.log()
+//}
+/* 
+                    let text1 = '<img src = ';
+                    let imgUrl1 = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=${key}`;
+                    let text21 = ` id=cd alt=${reference}></img>`;
+                    let totalText1 = text1 + imgUrl1 + text21;
+                    
+                    document.getElementById("photo").innerHTML = totalText1;*/
+/*********************************************************************************/
+function initialize() {
+    var input = document.getElementById('searchTextField');
+    new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
