5f40478486d14ad2a73bb2801d15c05e794e7313
Anuj Mathur <amathur@marchnetworks.com>
2020-09-01 20:24:29 -0400

    adding text + description with a different model

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,7 +1,11 @@
 let canvas = document.getElementById("gameCanvas");
 let context = canvas.getContext("2d");
+let canvas2 = document.getElementById("gameCanvas2");
+let context2 = canvas2.getContext("2d");
+let text;
 var loadFile = function (event) {
   context.clearRect(0, 0, canvas.width, canvas.height);
+  context2.clearRect(0, 0, canvas.width, canvas.height);
   let image = document.getElementById('file');
   image.src = URL.createObjectURL(event.target.files[0]);
 
@@ -12,29 +16,27 @@ var loadFile = function (event) {
   cocoSsd.load().then(model => {
     // detect objects in the image.
     model.detect(img).then(predictions => {
-      textArray = "Hmmm...I can find: ";
-      console.log(predictions);
-      
+
       for (let index = 0; index < predictions.length; index++) {
-        console.log(predictions[index].bbox[0], predictions[index].bbox[1]);
-        context.fillStyle ='rgba(0,225,0,0.2)';
-        
-        //context.fillRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);       
-        context.strokeRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);       
-        context.fillRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);       
-      
-        textArray += predictions[index].class;
-        index === predictions.length - 1 ? textArray += " " : textArray += ", ";
+
+        context.fillStyle = 'rgba(0,225,0,0.1)';
+        context2.fillStyle = '#1e4c76';
+
+        context2.font = "25px Arial";
+        context2.fillText(predictions[index].class, predictions[index].bbox[0], predictions[index].bbox[1] + 14);
+
+        context.strokeRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);
+        context.fillRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);
+
       }
-      document.getElementById("text").innerHTML = textArray;
+
     });
   });
 
   mobilenet.load().then(model => {
-    // Classify the image.
+    text = "Hmmm...I think the image is: ";
     model.classify(img).then(predictions => {
-      console.log('Predictions: ');
-      console.log(predictions);
+      document.getElementById("text").innerHTML = text + predictions[0].className;
     });
   });
 };
--- js/dist/chatbot.dev.js
+++ js/dist/chatbot.dev.js
@@ -2,9 +2,13 @@
 
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
+var canvas2 = document.getElementById("gameCanvas2");
+var context2 = canvas2.getContext("2d");
+var text;
 
 var loadFile = function loadFile(event) {
   context.clearRect(0, 0, canvas.width, canvas.height);
+  context2.clearRect(0, 0, canvas.width, canvas.height);
   var image = document.getElementById('file');
   image.src = URL.createObjectURL(event.target.files[0]);
   var htmlTag = "<img id=\"img\" src=\"".concat(image.src, "\" crossorigin='anonymous' width=\"500\" height=\"400\"/>");
@@ -13,27 +17,20 @@ var loadFile = function loadFile(event) {
   cocoSsd.load().then(function (model) {
     // detect objects in the image.
     model.detect(img).then(function (predictions) {
-      textArray = "Hmmm...I can find: ";
-      console.log(predictions);
-
       for (var index = 0; index < predictions.length; index++) {
-        console.log(predictions[index].bbox[0], predictions[index].bbox[1]);
-        context.fillStyle = 'rgba(0,225,0,0.2)'; //context.fillRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);       
-
+        context.fillStyle = 'rgba(0,225,0,0.1)';
+        context2.fillStyle = '#1e4c76';
+        context2.font = "25px Arial";
+        context2.fillText(predictions[index]["class"], predictions[index].bbox[0], predictions[index].bbox[1] + 14);
         context.strokeRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);
         context.fillRect(predictions[index].bbox[0], predictions[index].bbox[1], predictions[index].bbox[2], predictions[index].bbox[3]);
-        textArray += predictions[index]["class"];
-        index === predictions.length - 1 ? textArray += " " : textArray += ", ";
       }
-
-      document.getElementById("text").innerHTML = textArray;
     });
   });
   mobilenet.load().then(function (model) {
-    // Classify the image.
+    text = "Hmmm...I think the image is: ";
     model.classify(img).then(function (predictions) {
-      console.log('Predictions: ');
-      console.log(predictions);
+      document.getElementById("text").innerHTML = text + predictions[0].className;
     });
   });
 };
